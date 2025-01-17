5d2f93cf4b227b3f7eae56a521c7467e1c8e7f82
Samuel Lacerda <127802525+Samuel-Lacerda@users.noreply.github.com>
2025-01-17 05:26:35 -0300

    props e desestruturação de props

--- 3_AVANCANDO_NO_REACT/avancando/src/App.jsx
+++ 3_AVANCANDO_NO_REACT/avancando/src/App.jsx
@@ -2,10 +2,15 @@ import "./App.css";
 import ManageData from "./components/ManageData";
 import ListRenderer from "./components/ListRenderer";
 import CondicionalRender from "./components/CondicionalRender";
+import ShowUserName from "./components/ShowUserName";
+import CarDetails from "./components/CarDetails";
 
 import Paisagem from "./assets/2.png";
+import { useState } from "react";
 
 function App() {
+  const name = "Samuel";
+  const [username] = useState("Maria");
   return (
     <div className="App">
       <h1>Avançando em React</h1>
@@ -20,6 +25,10 @@ function App() {
       <ManageData />
       <ListRenderer />
       <CondicionalRender />
+      {/* props */}
+      <ShowUserName name={username} />
+      {/* destructuring */}
+      <CarDetails brand="VW" km={10000} color="blue" />
     </div>
   );
 }
